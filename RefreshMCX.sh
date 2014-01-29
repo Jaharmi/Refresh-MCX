3626a5edd9caa91ba9d613224c676fd20214dbf9
Richard Purves <rpurves@m-a6325.arts.local>
2014-01-29 08:53:58 +0000

    Update for 10.8/9 caching
    This update now kills the cfprefsd daemon and agent to stop any
    unexpected behaviour from it’s perpetual memory caching of preferences.
    
    This daemon will restart of it’s own accord and re-cache accordingly.

--- RefreshMCX.sh
+++ RefreshMCX.sh
@@ -5,7 +5,10 @@
 
 # Implemented : r.purves@arts.ac.uk
 # Version 1.0 : Initial Version
+# Version 1.1 : Now kills the cfprefsd process to make sure there's no preference caching going on
+# 				https://developer.apple.com/library/mac/#releasenotes/CoreFoundation/CoreFoundation.html
 
+killall cfprefsd
 rm -R /Library/Managed\ Preferences/*
 dscl . -delete /Computers
 dscl . -list Computers | grep -v "^localhost$" | while read computer_name ; do sudo dscl . -delete Computers/"$computer_name" ; done
