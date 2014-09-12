b3c1d085087cd9c54ae88aacba93af31aa2887bf
Richard Purves <franton@users.noreply.github.com>
2014-09-12 22:58:12 +0100

    Update RefreshMCX.sh
    Change of email address

--- RefreshMCX.sh
+++ RefreshMCX.sh
@@ -3,7 +3,7 @@
 # Script to flush and refresh MCX on a Casper Mac
 # Based on work found at jamfnation.jamfsoftware.com/discussion.html?id=5171
 
-# Implemented : r.purves@arts.ac.uk
+# Implemented : contact@richard-purves.com
 # Version 1.0 : Initial Version
 # Version 1.1 : Now kills the cfprefsd process to make sure there's no preference caching going on
 # 				https://developer.apple.com/library/mac/#releasenotes/CoreFoundation/CoreFoundation.html
@@ -12,7 +12,6 @@
 killall cfprefsd
 rm -Rfd /Library/Managed\ Preferences
 dscl . -mcxdelete /Computers/localhost
-dscl . -mcxdelete /Users/uadmin
 dscl . -mcxdelete /Users/$3
 
 jamf manage
